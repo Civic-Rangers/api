726c0f2eeaa830b5512105ab2a31775b835d84ce
Jorge Poveda <jpoveda731@gmail.com>
2024-09-21 05:31:58 -0400

    feat: shared img

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
-import routes from "./routers";
 import { mongoConnector } from "./utils/mongooseConnection";
+import express from "express";
+import path from "path";
+import routes from "./routers";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoConnector();
+
+app.use("/static", express.static(path.join(__dirname, "src/static")));
 
 app.use("/api", routes);
 
